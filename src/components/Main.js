099be557f575534cd82e5f2122809214d2ed6d70
KriszProg <krisz.codecool@gmail.com>
2021-02-17 09:12:57 +0100

    implement basic layout of CVForm

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Main.css'
+
+function Main() {
+    return (
+        <div className='main-container'>
+            <h1>Welcome in CV Creator!</h1>
+        </div>
+    )
+}
+
+export default Main;
---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../NavBar.css';
+
+function NavBar() {
+    return (
+        <header className='navbar-header'>
+            <ul className='navbar-list'>
+                <li className='navbar-list-item'>
+                    <Link className='navbar-link' to='/'>HOME</Link>
+                </li>
+                <li className='navbar-list-item'>
+                    <Link className='navbar-link' to='/add'>CREATE CV</Link>
+                </li>
+                <li className='navbar-list-item'>
+                    <Link className='navbar-link' to='/about'>ABOUT</Link>
+                </li>
+            </ul>
+        </header>
+    )
+}
+
+export default NavBar;
