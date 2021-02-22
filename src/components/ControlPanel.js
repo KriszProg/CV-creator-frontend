1039a70b37795ad6808c76342024bc7b856aa325
KriszProg <krisz.codecool@gmail.com>
2021-02-22 08:21:03 +0100

    add ControlPanel to CVForm for handling CV Title

---
+++ src/components/ControlPanel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../CVForm.css';
+
+function ControlPanel(props) {
+    const {id, title, creationDate} = props.cv; 
+
+    return (
+        <div className='grid-box' id='control-panel'>
+            <div id='title-controller-box'>
+                <label htmlFor='cv-title' id='cv-title-label'>Title: </label>
+                <textarea className='textarea title' id='cv-title' name='cv-title' rows='1' max-rows='1' maxLength='18' defaultValue={title}></textarea>
+            </div>
+        </div>
+    )
+}
+
+export default ControlPanel;
