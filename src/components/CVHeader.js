3f28964dc74d21b426c95da49eff3d1b5015d2e5
KriszProg <krisz.codecool@gmail.com>
2021-02-25 13:20:34 +0100

    display fecthed content in 'ControlPanel' and 'CVHeader'

--- src/components/CVHeader.js
+++ src/components/CVHeader.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ImgUploader from './ImgUploader';
 import '../CVForm.css';
 
-function CVHeader() {
+function CVHeader(props) {
+    const {name, role} = props.candidate !== null ? props.candidate : '';
+
     return (
         <div className='grid-box' id="cv-header">
             <ImgUploader element='background-img'/>
             <div id='cv-header-candidate'>
-                <textarea className='textarea name' id='candidate-name' name='candidate-name' rows='1' max-rows='1' maxLength='18'></textarea>
-                <textarea className='textarea role' id='candidate-role' name='candidate-role' rows='1' max-rows='1' maxLength='30'></textarea>
+                <textarea className='textarea name' id='candidate-name' name='candidate-name' rows='1' max-rows='1' maxLength='18' defaultValue={name}></textarea>
+                <textarea className='textarea role' id='candidate-role' name='candidate-role' rows='1' max-rows='1' maxLength='30' defaultValue={role}></textarea>
             </div>
             <ImgUploader element='profile-photo'/>
         </div>
