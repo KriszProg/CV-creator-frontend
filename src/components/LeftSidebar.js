816021544ba7295bd69cdb7609afc94f9b746834
KriszProg <krisz.codecool@gmail.com>
2021-03-02 12:24:01 +0100

    modify saving logic (fetch CV after save a section content)

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -1,22 +1,22 @@
-import React, { useEffect } from 'react';
-import InputSectionSimple from './InputSectionSimple';
-import '../CVForm.css';
+import React from 'react';
 import SectionContact from './SectionContact';
+import SectionSingleData from './SectionSingleData';
+import '../CVForm.css';
 
 function LeftSidebar(props) {
     const id = props.fetchedCV.cvIdentifiers.id;
-    // const {email, phoneNr, linkedInProfile} = props.fetchedCV.contact !== null ? props.fetchedCV.contact : '';
+    const saveInput = props.saveInput;
     const {selfDefinition} = props.fetchedCV.selfDefinition !== null ? props.fetchedCV.selfDefinition : '';
     const {strength} = props.fetchedCV.strength !== null ? props.fetchedCV.strength : '';
     const {mentorOpinion} = props.fetchedCV.mentorOpinion !== null ? props.fetchedCV.mentorOpinion : '';
 
     return (
         <div className='sidebar' id='left-sidebar'>
-            <SectionContact CVId={id} contact={props.fetchedCV.contact}/>
+            <SectionContact CVId={id} contact={props.fetchedCV.contact} saveInput={props.saveInput} />
             <div className="bottom-section">
-                <InputSectionSimple CVId={id} sectionTitle='Self Definition' object={selfDefinition} />
-                <InputSectionSimple CVId={id} sectionTitle='Strength' object={strength} />
-                <InputSectionSimple CVId={id} sectionTitle='Mentor Opinion' object={mentorOpinion} />
+                <SectionSingleData CVId={id} sectionTitle='Self Definition' object={selfDefinition} saveInput={saveInput} />
+                <SectionSingleData CVId={id} sectionTitle='Strength' object={strength} saveInput={saveInput} />
+                <SectionSingleData CVId={id} sectionTitle='Mentor Opinion' object={mentorOpinion} saveInput={saveInput} />
             </div>
         </div>
     )
